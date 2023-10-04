50956d825710227472e761458ef11d83d7388ddf
Falseness <baharevuniversal@gmail.com>
2023-10-04 23:40:49 +0300

    feat subsets-ii faster solution with worse code

--- subsets-ii/solution.hpp
+++ subsets-ii/solution.hpp
@@ -1,37 +1,37 @@
 /*
-Перебираем все битовые маски. По ним строим подмножества.
-Это O(2^num.size() * num.size()) времени.
+Сначала считаем количество вхождений каждого числа.
+Перебираем числа
+Пусть дошли до числа x. Оно встречается k раз. 
+уже построили все подмножества из чисел < x по предположению индукции.
+Тогда для перехода нужно добавить ко всем старым подмножествам число x
+0, 1, 2, ..., k раз.
 
-Далее дедублицируем за O(klog(k) * num.size()) где k = subsets.size()
+чтобы уменьшить константу времени, можно добавить сначала элементы с 1 x'ом, потом
+по ним построить с 2 x'ами, и т.д.
+
+Ещё решение не использует рекурсию, что тоже улучшает время, хотя на таком размере данных это незаметно.
+асимптотика O(kMaximumNumValue - kMinimumNumValue) + O(размер ответа).  
 */
 class Solution {
-    // придерживаюсь того же codestyle
-    static bool getBit(const int mask, const int index) {
-        return (mask >> index) & 1;
-    }
-    static multiset<int> createSubsetBy(const int mask,
-            const vector<int>& nums) {
-        multiset<int> subset;
-        for (size_t i = 0; i < nums.size(); ++i) {
-            if (getBit(mask, i)) {
-                subset.insert(nums[i]);
-            }
-        }
-        return subset;
-    }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<multiset<int>> subsets;
-        for (size_t mask = 0; mask < (1 << nums.size()); ++mask) {
-            subsets.push_back(createSubsetBy(mask, nums));
+        vector<vector<int>> result = {{}};
+        static constexpr int kMaximumNumValue = 10;
+        static constexpr int kMinimumNumValue = -10;
+        static_assert(kMinimumNumValue <= kMaximumNumValue);
+        std::array<size_t, kMaximumNumValue - kMinimumNumValue + 1> occurences_count = {0};
+        // думаю не нужно заменять 3 строчки на stl алгоритм
+        for (int number : nums) {
+            ++occurences_count[number - kMinimumNumValue];
         }
-        ranges::sort(subsets);
-        // с ranges это выглядит не лучше
-        subsets.erase(unique(begin(subsets), end(subsets)), end(subsets));
-        vector<vector<int>> result;
-        // возможно тут можно воспользоваться stl, но зачем?
-        for (auto&& subset : subsets) {
-            result.push_back({subset.begin(), subset.end()});
+        for (int shifted_number = 0; shifted_number < occurences_count.size();
+                ++shifted_number) { 
+            int number = shifted_number + kMinimumNumValue;
+            size_t iterations_count = result.size() * occurences_count[shifted_number];
+            for (size_t i = 0; i < iterations_count; ++i) {
+                result.push_back(result[i]);
+                result.back().push_back(number);
+            }
         }
         return result;
     }
