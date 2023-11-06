cb4a11922e76b23d05b7f79670baeedacf3b539e
Falseness <baharevuniversal@gmail.com>
2023-11-06 13:51:50 +0300

    feat fix trapping-rain-water

--- trapping-rain-water/solution.hpp
+++ trapping-rain-water/solution.hpp
@@ -1,40 +1,25 @@
 /*
-Пусть left_maximum - максимум на префиксе.
-Тогда до первого следующего столбца >= left_maximum вода
-будет на уровне left_maximum.
-Если же нет следующего столбца >= left_maximum, значит мы нашли самый правый глобальный максимум.
-Для столбцов правее него применимы те же рассуждения, только с другого конца.
-Можно просто сделать reverse и применить тот же алгоритм.
-Время работы - O(height.size())
-
-Через views это написать нельзя, т.к. leetcode не поддерживает.
-Пробовал это переписать через ranges, но чек ranges походу тоже криво работают.
-например, нельзя сделать
-std::ranges::subrange new_range(ranges::begin(height), ranges::end(height));
-хотя должно быть можно
+Решение, похожее на предыдущее. 
+Используем два указателя. Поддерживаем, на префиксе
+или на суффиксе самый высокий столбец. Например, если
+на префиксе столбец меньше, то можем обновить результат на префиксе
+(аналогично как предыдущем решении).
 */
 class Solution {
 public:
-    using Iterator = vector<int>::iterator;
     int trap(vector<int>& height) {
-        auto [result, last_left_maximum] = calculateOneDirectionTrap(height.begin(), height.end());
-        reverse(last_left_maximum, height.end());
-        result += calculateOneDirectionTrap(last_left_maximum, height.end()).first;
+        int result = 0;
+        size_t l = 0;
+        size_t r = height.size() - 1;
+        int l_maximum = 0;
+        int r_maximum = 0;
+        while (l < r) {
+            l_maximum = std::max(l_maximum, height[l]);
+            r_maximum = std::max(r_maximum, height[r]);
+            result += l_maximum < r_maximum ? l_maximum - height[l++] :
+                                              r_maximum - height[r--];  
+        }
         return result;
     }
-    std::pair<int, Iterator> calculateOneDirectionTrap(Iterator begin, Iterator end) {
-        int result = 0;
-        Iterator left_maximum = end;
-        int sum = 0;
-        for (auto it = begin; it != end; ++it) {
-            if (left_maximum != end && *it < *left_maximum) {
-                sum += *left_maximum - *it;
-                continue;
-            }
-            result += sum;
-            left_maximum = it;
-            sum = 0;
-        }
-        return {result, left_maximum};
-    }
 };
+
