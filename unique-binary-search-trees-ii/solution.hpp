e1334bcd48321ddf1f19152559755ed3425e29a1
baharev nikita <baharevuniversal@gmail.com>
2023-12-24 15:22:03 +0300

    feat added cache in unique-binary-search-trees-ii

--- unique-binary-search-trees-ii/solution.hpp
+++ unique-binary-search-trees-ii/solution.hpp
@@ -12,18 +12,22 @@
 /*
 generateTrees рекурсивно генерирут деревья со всеми значениями вершин [minimum, maximum]
 выбор root'а однозначно задает набор вершин в левом поддереве и в правом.
-
 Время, память - O(размера результата)
 */
 class Solution {
-    vector<TreeNode*> generateTrees(int minimum, int maximum) {
+    static vector<TreeNode*> kEmptyTree;
+    vector<TreeNode*>& generateTrees(int minimum, int maximum) {
         if (minimum > maximum) {
-            return {nullptr};
+            return kEmptyTree;
         }
-        vector<TreeNode*> result;
+        auto& result = found_tree_[minimum - 1][maximum - 1]; 
+        if (!result.empty()) {
+            return result;
+        }
         for (int root = minimum; root <= maximum; ++root) {
-            auto left_childs = generateTrees(minimum, root - 1);
-            auto right_childs = generateTrees(root + 1, maximum);
+            auto& left_childs = generateTrees(minimum, root - 1);
+            auto& right_childs = generateTrees(root + 1, maximum);
+
             for (auto left_child : left_childs) {
                 for (auto right_child: right_childs) {
                     result.push_back(new TreeNode(root, left_child, right_child));
@@ -32,8 +36,15 @@ class Solution {
         }
         return result;
     }
+    vector<vector<vector<TreeNode*>>> found_tree_;
 public:
     vector<TreeNode*> generateTrees(int n) {
-        return generateTrees(1, n);
+        found_tree_.resize(n, vector<vector<TreeNode*>>(n));
+        auto result = generateTrees(1, n); 
+        found_tree_.clear();
+        return result;
     }
 };
+
+vector<TreeNode*> Solution::kEmptyTree = {nullptr};
+
